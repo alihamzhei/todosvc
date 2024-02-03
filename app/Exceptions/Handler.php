58123f1b6278da61af8ab9159891028ad53be315
alihamzehei <alihamzehei2017@gmail.com>
2024-02-03 17:50:09 +0330

    fixed the error handler in handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,17 @@
 
 namespace App\Exceptions;
 
+use App\Facades\Response;
+use GuzzleHttp\Exception\ClientException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\UnauthorizedException;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+use Symfony\Component\HttpFoundation\Response as ResponseMain;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -21,10 +31,66 @@ class Handler extends ExceptionHandler
     /**
      * Register the exception handling callbacks for the application.
      */
-    public function register(): void
+    public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        if ($e instanceof HttpException) {
+            $statusCode = $e->getStatusCode();
+            $message = ResponseMain::$statusTexts[$statusCode];
+
+            return Response::message($message)
+                ->send($statusCode);
+        }
+
+        if ($e instanceof ModelNotFoundException) {
+            return Response::message('global.errors.not_found')
+                ->send(ResponseMain::HTTP_NOT_FOUND);
+        }
+
+        if ($e instanceof BadRequestException) {
+            return Response::message($e->getMessage())
+                ->send(ResponseMain::HTTP_BAD_REQUEST);
+        }
+
+        if ($e instanceof AuthorizationException) {
+            return Response::message($e->getMessage())
+                ->send(ResponseMain::HTTP_FORBIDDEN);
+        }
+
+        if ($e instanceof UnauthorizedException) {
+            return Response::message($e->getMessage())
+                ->send(ResponseMain::HTTP_FORBIDDEN);
+        }
+
+        if ($e instanceof AuthenticationException) {
+            return Response::message($e->getMessage())
+                ->send(ResponseMain::HTTP_UNAUTHORIZED);
+        }
+
+        if ($e instanceof ValidationException) {
+            $errors = $e->validator->errors()->messages();
+
+            return Response::errors($errors)
+                ->message($e->getMessage())
+                ->send(ResponseMain::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        if ($e instanceof ClientException) {
+            $errors = $e->getResponse()->getBody();
+            $code = $e->getCode();
+
+            return Response::errors($errors)
+                ->send($code);
+        }
+
+        if (env('APP_DEBUG', false)) {
+            return parent::render($request, $e);
+        }
+
+        return Response::message('Unexpected Error , try later please')
+            ->send(ResponseMain::HTTP_INTERNAL_SERVER_ERROR);
     }
 }
